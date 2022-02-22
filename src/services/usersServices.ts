2caf2e9f21429f78c77ec755c80fe64b725cf960
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-22 20:35:41 -0300

    Retorna Token

--- src/services/usersServices.ts
+++ src/services/usersServices.ts
@@ -1,12 +1,15 @@
 import UserBody from '../interface/User';
 import validations from '../middlewares/validations';
+import * as auth from '../middlewares/auth';
 
+// export default create = async (userCreated: UserBody) => {
 export default function create(userCreated: UserBody) {
   const isValidation = validations.validate(userCreated);
   if (isValidation.error) { 
     const messageError = isValidation.error.details[0].message;
     return { code: 400, message: messageError }; 
   }
+  const isAuth = auth.create(userCreated.username);
   
-  return { code: 201, message: userCreated };
+  return { code: 201, message: isAuth };
 }
