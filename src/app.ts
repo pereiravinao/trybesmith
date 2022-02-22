78aa7eac273efb62b8fe586b621d9dc99e502674
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-22 20:06:47 -0300

    Iniciando rota para cadastro de usuario

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import usersController from './controllers/usersController';
 
 const app = express();
 
 app.use(express.json());
+
+app.post('/users', usersController);
 
 export default app;
