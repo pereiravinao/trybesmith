c5d9792afb8bfb4c50de311eab0911a3f39bcc7a
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-22 20:35:20 -0300

    Gerando token na criacao

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+
+const secretJwt = process.env.JWT_SECRET || 'teste123';
+
+export const create = (username: string) => {
+  // const jwtConfig = {
+  //   expiresIn: '2h',
+  //   algorithm: 'HS256',
+  // };
+  const token = jwt.sign({ username }, secretJwt);
+  return token;
+};
+
+export const verify = (token: string) => {
+  try {
+    const decode = jwt.verify(token, secretJwt);
+    return decode;
+  } catch (err) {
+    const error = new Error();
+    // error.message = 'Expired or invalid token';
+    // error.code = '401';
+    return error;
+  }
+};
+
+// export const authentication = (token: string) => {
+//   if (!token) { return { code: 401, message: 'Token not found' }; }
+
+//   const response = verify(token);
+//   if (response.code === '401') { return { code: 401, message: 'Expired or invalid token' }; }
+
+//   return response.email;
+// };
