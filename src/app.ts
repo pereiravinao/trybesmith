a742a1dafd361c115e1295fa5a5531a6099939c6
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-23 16:32:07 -0300

    Altrando validacao do login para o App

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as usersController from './controllers/usersController';
 import * as LoginController from './controllers/LoginController';
 import * as productsController from './controllers/productsController';
+import * as auth from './middlewares/auth';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.post('/users', usersController.create);
 app.post('/login', LoginController.login);
-app.post('/products', productsController.create);
+
+app.post('/products', auth.authentication, productsController.create);
 
 export default app;
