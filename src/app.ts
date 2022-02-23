abe4a9d345d9d8c21a73e4c39d8911fb728bf81b
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-23 12:12:13 -0300

    Criando rota para Login

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import * as usersController from './controllers/usersController';
+import * as LoginController from './controllers/LoginController';
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/users', usersController.create);
+app.post('/login', LoginController.login);
 
 export default app;
