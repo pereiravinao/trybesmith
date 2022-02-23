df0b799317f22a87877f2fb2ab026bdc896597c2
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-23 14:47:44 -0300

    valida login e senha no BD

--- src/services/loginServices.ts
+++ src/services/loginServices.ts
@@ -1,4 +1,5 @@
 import * as validations from '../middlewares/validations';
+import * as users from '../models/users';
 import * as auth from '../middlewares/auth';
 import UserLogin from '../interface/UserLogin';
 
@@ -12,6 +13,12 @@ export const login = async (user: UserLogin) => {
     const removeQuotes = message.replace(/"/g, '');
     const newMessage = removeQuotes.charAt(0).toUpperCase() + removeQuotes.slice(1);
     return { code, message: { error: newMessage } }; 
+  }
+
+  const validLogin = await users.getByLogin(user);
+  
+  if (!Object.keys(validLogin).length) {
+    return { code: 401, message: { error: 'Username or password invalid' } };
   }
 
   const isAuth = auth.create(user.password);
