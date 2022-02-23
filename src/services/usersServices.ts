fe05f3ba1c502bf123957d36948bb25024a0ac45
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-23 11:33:45 -0300

    Implementado toda a logica

--- src/services/usersServices.ts
+++ src/services/usersServices.ts
@@ -2,14 +2,17 @@ import UserBody from '../interface/User';
 import validations from '../middlewares/validations';
 import * as auth from '../middlewares/auth';
 
-// export default create = async (userCreated: UserBody) => {
-export default function create(userCreated: UserBody) {
+export const create = async (userCreated: UserBody) => {
   const isValidation = validations.validate(userCreated);
+  
   if (isValidation.error) { 
-    const messageError = isValidation.error.details[0].message;
-    return { code: 400, message: messageError }; 
+    const { message } = isValidation.error.details[0];
+    const code = message.includes('is required') ? 400 : 422;    
+    return { code, message }; 
   }
   const isAuth = auth.create(userCreated.username);
   
   return { code: 201, message: isAuth };
-}
+};
+
+export const removeWarnLint = () => {};
