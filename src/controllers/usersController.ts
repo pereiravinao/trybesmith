57a0443e8ab000366fd462263a367b176bf6c947
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-23 11:51:31 -0300

    Corrigindo retorno da message

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
 import * as usersServices from '../services/usersServices';
 import UserBody from '../interface/User';
-import Validate from '../interface/Validate';
 
 export const create = async (req: Request, res: Response) => {
   const userCreated: UserBody = req.body;
-  const { code, message }: Validate = await usersServices.create(userCreated);
+  const { code, message } = await usersServices.create(userCreated);
+  console.log(code, message);
   
   return res.status(code).json(message);
 };
