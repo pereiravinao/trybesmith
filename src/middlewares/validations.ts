e826920784ca9846a2ce8e54d071f3918d66c2a5
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-23 11:33:05 -0300

    Finalizando JOI

--- src/middlewares/validations.ts
+++ src/middlewares/validations.ts
@@ -1,23 +1,16 @@
 import Joi from 'joi';
 
-const newUser = Joi.object({
-  username: Joi.string()
-    .required()
-    .$.min(3).rule({ message: 'Username must be longer than 2 characters' }),
+const schemeNewUser = Joi.object({
+  username: Joi.string().required().min(3).messages({
+    'string.min': '"username" must be longer than 2 characters',
+  }),
+  classe: Joi.string().required().min(3).messages({
+    'string.min': '"classe" must be longer than 2 characters',
+  }),
+  level: Joi.number().required().greater(0),
+  password: Joi.string().required().min(8).messages({
+    'string.min': '"password" must be longer than 7 characters',
+  }),
+}).strict();
 
-  password: Joi.string()
-    .required()
-    .$.min(8).max(10).rule({ message: 'Password must be longer than 7 characters' }),
-
-  classe: Joi.string()
-    .required()
-    .$.min(2).rule({ message: 'Classe must be longer than 2 characters' }),
-
-  level: Joi.number()
-    .required()
-    .$.min(1).rule({ message: 'Level must be greater than 0' }),
-
-  image: Joi.string().allow(),
-});
-
-export default newUser;
+export default schemeNewUser;
