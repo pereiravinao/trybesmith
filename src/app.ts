5e796b761ce36fdfc2b2da05725bcf98d2cb461e
Vinicius Pereira Norberto <vpereira.norberto@gmail.com>
2022-02-24 10:42:16 -0300

    Rota para criacao de ORDER

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as usersController from './controllers/usersController';
 import * as LoginController from './controllers/LoginController';
 import * as productsController from './controllers/productsController';
+import * as orderController from './controllers/orderController';
 import * as auth from './middlewares/auth';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.post('/users', usersController.create);
 app.post('/login', LoginController.login);
+app.post('/orders', auth.authentication, orderController.create);
 
 app.route('/products')
   .post(auth.authentication, productsController.create)
